eadf5975239d347181cf302572fa36815e414430
Ubuntu <ubuntu@ip-172-31-33-150.eu-west-1.compute.internal>
2013-07-17 21:32:37 +0000

    Deleting buffer in web.js

--- web.js
+++ web.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var fs      = require('fs');
 var index   = fs.readFileSync('./index.html', 'utf8');
-var buffer  = new Buffer(index,'utf8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf8',0,2129));
+  response.send(index.toString());
 });
 
 var port = process.env.PORT || 8080;
